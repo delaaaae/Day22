72d79007e9ae8c2835b3006235e31bc6075072b9
unknown <delic.amir09@gmail.com>
2021-03-30 01:33:55 +0200

    Initial import

---
+++ ex07/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Box from './Box';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Box />
+      </header>
+    </div>
+  );
+}
+
+export default App;
