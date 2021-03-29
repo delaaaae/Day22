f993c1d6c4e4da23ec8b0e569a4f110c4adefcd0
unknown <delic.amir09@gmail.com>
2021-03-29 23:14:27 +0200

    Initial import

---
+++ ex03/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './Plant';
+
+function App() {
+  render() {
+  return (
+    <div className="App">
+      <Plant />
+    </div>
+  );
+  }
+}
+
+export default App;
