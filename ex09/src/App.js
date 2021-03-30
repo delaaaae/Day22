69fbe16a0ea89a6c77cf1566d472e8a71137b6fa
unknown <delic.amir09@gmail.com>
2021-03-30 03:12:51 +0200

    Initial import

---
+++ ex09/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Flower from './Flower';
+
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Flower color = "red"/>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
