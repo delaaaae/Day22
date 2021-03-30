150d0bd3993f018aec31de00448e6d893b13de92
unknown <delic.amir09@gmail.com>
2021-03-30 04:00:26 +0200

    Initial import

---
+++ ex10/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Box from './Box';
+
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Box size="20"/>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ ex10/src/Box.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Box extends Component {
+        render() {
+            
+            return (
+                <h2>This is a {this.props.size}px Box</h2>
+            )
+        }
+}
+
+
+
+export default Box;
